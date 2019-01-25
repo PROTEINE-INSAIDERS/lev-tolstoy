780f9549cf502fdffbfcc6a2a1a0a5dc7648382f
Stanislav Chernichkin <schernichkin@gmail.com>
2019-01-25 23:07:26 +0300

    wip

--- src/Lev/Reader/Static.hs
+++ src/Lev/Reader/Static.hs
@@ -1,19 +1,51 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE CPP
+           , DataKinds
+           , KindSignatures
+           , Rank2Types
+           , ScopedTypeVariables
+           , TypeFamilies
+           , TypeOperators 
+           , TypeApplications
+#-}
 
 module Lev.Reader.Static where
 
 import           Control.Exception
 import           Control.Monad.Primitive
+import           Control.Monad.ST
 import           Data.Primitive
+import           Data.Singletons
+import           Data.Singletons.Prelude.Num
+import           Data.Singletons.TypeLits
+import           Data.Typeable
 import           Data.Word
 
-data Result a = Done {-# UNPACK #-} !a
-              | Fail {-# UNPACK #-} !SomeException
+#include "MachDeps.h"
 
-newtype Reader m a = Reader { runReader :: forall b . Addr -> (Addr -> b -> m (Result a)) -> m a }
+data Result a = Done {-# UNPACK #-} !a
+              | Fail {-# UNPACK #-} !SomeException deriving (Show, Typeable)
+
+newtype Reader (o :: Nat) (s :: Nat) m a = Reader 
+    { runReader :: forall r . Addr -> (a -> m (Result r)) -> m (Result r) }
+
+{-# INLINABLE pureReader #-}
+pureReader :: a -> Reader o 0 m a
+pureReader a = Reader $ \addr k -> k a
+
+{-# INLINABLE bindReader #-}
+bindReader :: ((oa + sa) ~ ob) => Reader oa sa m a -> (a -> Reader ob sb m b) -> Reader oa (sa + sb) m b
+bindReader (Reader f) k = Reader $ \addr k -> do
+    undefined
+
+-- TODO: DO NOT EXPOSE!! (otherwise introduce sizeof which is not safe though)
+{-# INLINE readPrim #-}
+readPrim :: forall o s m a . (KnownNat o, PrimMonad m, Prim a) => Reader o s m a 
+readPrim = Reader $ \addr k -> readOffAddr (addr `plusAddr` off) 0 >>= k
+    where
+        off = fromIntegral $ natVal $ sing @o
 
 {-# INLINABLE readWord8 #-}
-readWord8 :: (PrimMonad m) => Reader m Word8
-readWord8 = Reader $ \addr k -> do
-    a <- readOffAddr addr 0
-    return a
+{-# SPECIALISE readWord8 :: (KnownNat o) => Reader o SIZEOF_WORD8 IO Word8 #-}
+{-# SPECIALISE readWord8 :: (KnownNat o) => Reader o SIZEOF_WORD8 (ST s) Word8 #-}
+readWord8 :: (KnownNat o, PrimMonad m) => Reader o SIZEOF_WORD8 m Word8
+readWord8 = readPrim
