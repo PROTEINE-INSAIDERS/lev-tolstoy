b12e5c92dbd877f6b61928ee69b42ce4b7fb618d
Stanislav Chernichkin <schernichkin@gmail.com>
2019-02-06 00:26:48 +0300

    wip

--- bin/bench.sh
+++ bin/bench.sh
@@ -1,1 +1,1 @@
-stack bench --profile --benchmark-arguments='--regress allocated:iters --regress numGcs:iters --output bench.html +RTS -T -hc -p -K100M'
+stack bench --benchmark-arguments='--regress allocated:iters --regress numGcs:iters --output bench.html +RTS -T'
