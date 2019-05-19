138006a66734a9d918b18fa863e70fe68c3ab8bb
Stanislav Chernichkin <schernichkin@gmail.com>
2019-05-19 23:48:53 +0300

    wip

--- examples/nanodump/Main.hs
+++ examples/nanodump/Main.hs
@@ -1,10 +1,9 @@
-data Result = Done | Fail 
+data Result = Done {-# UNPACK #-} !Int | Fail 
 
-{-# NOINLINE ask #-}
 ask :: Int -> Result
-ask a = if a == 42 then Done else Fail
+ask a = if (a /= 42) then Done a else Fail
 
 main :: IO ()
-main = case (ask 42) of
-    Done -> putStrLn "done."
+main = case (ask 43) of
+    Done _ -> putStrLn "done"
     Fail -> putStrLn "error!"
